f6d493eb5a6e795f404265d5db46806764b0306d
Jake-Purton <jakecapurton@gmail.com>
2023-02-20 09:17:36 +0000

    comment

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use platform::PlatformPlugin;
 use player::PlayerPlugin;
 
 const FELLA_SPRITE: &str = "fella.png";
-const SPRITE_SCALE: f32 = 1.0;
+const SPRITE_SCALE: f32 = 0.707106;
 const FELLA_SPRITE_SIZE: Vec2 = Vec2::new(64.0 * SPRITE_SCALE, 64.0 * SPRITE_SCALE);
 const GRAVITY_CONSTANT: f32 = -2800.0;
 
@@ -42,3 +42,5 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             player: asset_server.load(FELLA_SPRITE)
         });
 }
+
+// make a basic ascii map file that i can use to create walls etc.
